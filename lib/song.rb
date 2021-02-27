8ab3425e14311489f07ffd280fbcb6b35da7f259
Kristin <kristinponsonby@gmail.com>
2021-02-27 14:05:42 -0500

    commit tree dd3b9bf3880fe720cacceb30a6b4e52b8aecc12f

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,6 @@
+require "pry"
 class Song
+
   attr_accessor :name, :artist_name
   @@all = []
 
@@ -8,6 +10,65 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name=(name)
+    song
+  end 
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name=(name)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if !song
+        song = self.create_by_name(name) 
+    end
+    song
+  end
+
+  def self.alphabetical
+    @@all.sort_by { |song| song.name }
+end
+
+  def self.new_from_filename(file_name)
+        data = file_name.split(" - ")
+        artist_name = data[0]
+        name = data[1]
+        song = self.new
+        song.artist_name = artist_name
+        song.name = name.delete_suffix(".mp3")
+        song
+  end
+
+  def self.create_from_filename(file_name)
+    data = file_name.split(" - ")
+    artist_name = data[0]
+    name = data[1]
+    song = self.new
+    song.artist_name = artist_name
+    song.name = name.delete_suffix(".mp3")
+    @@all << song
+  end
+
+  def self.destroy_all
+    @@all.clear
   end
 
 end
